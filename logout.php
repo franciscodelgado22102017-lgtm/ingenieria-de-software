917fa0fad730bc66a17381f1de564f8bb0bb48c2
Francisco Delgado Salazar <franciscodelgado22102017@gmail.com>
2026-05-06 13:16:14 -0400

    cookies-3

--- logout.php
+++ logout.php
@@ -1,7 +1,20 @@
 <?php
+// logout.php - Destruir sesión y eliminar cookies
 session_start();
-session_unset();    // Elimina las variables
-session_destroy();  // Destruye la sesión
-header("Location: index.html");
+
+// Destruir la sesión
+session_unset();
+session_destroy();
+
+// Eliminar las cookies si existen
+$cookie_names = ['id_usuario', 'username', 'user_email'];
+foreach ($cookie_names as $name) {
+    if (isset($_COOKIE[$name])) {
+        setcookie($name, "", time() - 3600, "/");
+    }
+}
+
+// Redirigir al login
+header("Location: index.php");
 exit();
 ?>
